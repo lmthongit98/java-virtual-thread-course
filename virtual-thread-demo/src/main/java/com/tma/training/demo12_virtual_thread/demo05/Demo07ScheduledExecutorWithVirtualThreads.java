a4cd933493a2409214eddda22b300750edb7b894
lmthongit98 <75925571+lmthongit98@users.noreply.github.com>
2025-03-08 17:14:28 +0700

    demo_12: scheduler executor with virtual threads

---
+++ virtual-thread-demo/src/main/java/com/tma/training/demo12_virtual_thread/demo05/Demo07ScheduledExecutorWithVirtualThreads.java
@@ -0,0 +1,47 @@
+package com.tma.training.demo12_virtual_thread.demo05;
+
+import com.tma.training.util.CommonUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/*
+    The JDK has no built-in task-scheduling tool that uses virtual threads.
+    Thus, we must write a work around that use Platform Thread to schedule and Virtual Thread to execute.
+
+    Reference: https://stackoverflow.com/questions/76587253/how-to-use-virtual-threads-with-scheduledexecutorservice
+*/
+
+public class Demo07ScheduledExecutorWithVirtualThreads {
+
+    private static final Logger log = LoggerFactory.getLogger(Demo07ScheduledExecutorWithVirtualThreads.class);
+
+    public static void main(String[] args) {
+        scheduledWithVT();
+
+        CommonUtils.sleep(Duration.ofSeconds(5)); // prevent main thread from exiting from 5s
+    }
+
+    private static void scheduled() {
+        try (var executor = Executors.newSingleThreadScheduledExecutor()) {
+            executor.scheduleAtFixedRate(() -> {
+                log.info("executing task");
+            }, 0, 1, TimeUnit.SECONDS);
+        }
+    }
+
+    // offloading the work to virtual threads, and schedule the work with a scheduler.
+    private static void scheduledWithVT() {
+        var scheduler = Executors.newSingleThreadScheduledExecutor();
+        var executor = Executors.newVirtualThreadPerTaskExecutor();
+        try (scheduler; executor) {
+            scheduler.scheduleAtFixedRate(() -> {
+                executor.submit(() -> log.info("executing task"));
+            }, 0, 1, TimeUnit.SECONDS);
+        }
+    }
+
+}
